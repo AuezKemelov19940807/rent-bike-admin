50d1373b7ed862df676f3655bafd46ead101bf30
Auez <kemelove@bk.ru>
2026-04-14 20:01:44 +0600

    fix login

--- app/middleware/auth.global.ts
+++ app/middleware/auth.global.ts
@@ -1,10 +1,22 @@
+// export default defineNuxtRouteMiddleware((to) => {
+//   if (to.path === "/login" || to.path === "/register") return;
+
+//   const token = useCookie("auth_token");
+
+//   if (!token.value) {
+//     abortNavigation();
+//     return navigateTo("/login");
+//   }
+// });
+
 export default defineNuxtRouteMiddleware((to) => {
-  if (to.path === "/login" || to.path === "/register") return;
+  const publicPages = ["/login", "/register"];
+
+  if (publicPages.includes(to.path)) return;
 
   const token = useCookie("auth_token");
 
   if (!token.value) {
-    abortNavigation();
     return navigateTo("/login");
   }
 });
